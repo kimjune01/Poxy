913f132fe1ae88ea968f4bca8bd1493d4738e5c4
June <kimjune01@gmail.com>
2017-07-23 15:09:25 -0700

    authenticate controller render works now. passing skinny testing

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,11 +1,11 @@
 class AuthenticationsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create, :update, :auth] #for dev only, disables authenticity checking on create/update
 
-  #POST /authentication
+  #POST /authenticate
   def auth
     params.require(:authentication).permit([:user_id, :session_token])
     if sessionValid?
-      render head :ok
+      head :ok
     else
       render :json => { error: "Invalid authentication credentials"}, :status => :bad_request
     end
@@ -18,7 +18,6 @@ class AuthenticationsController < ApplicationController
     if user
       return user.session_token == token
     end
-
     return false
   end
 
