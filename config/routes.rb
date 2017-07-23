e267d069b9b6ef6ca601fb45864132018e6ea71b
June <kimjune01@gmail.com>
2017-07-23 16:40:59 -0700

    added sign up controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
 
   post '/authenticate', to: 'authentications#auth'
-
-
+  post '/signup',       to: 'signup#signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
