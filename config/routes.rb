a87877dcd93178d2a006ffe22532e352d878b59a
June <kimjune01@gmail.com>
2017-07-30 16:26:08 -0700

    rename to seshcontroller to address namespace error

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   post '/authenticate', to: 'authentications#auth'
   post '/signup',       to: 'signup#signup'
-
+  #post '/login', 		to: 'sesh#create'
   devise_scope :user do
-    post '/login', to: 'devise/sessions#create'
-    get '/login',  to: 'devise/sessions#new'
+    post '/login', to: 'sesh#create'
+    get '/login',  to: 'sesh#new'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
