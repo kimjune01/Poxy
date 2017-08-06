a10c5881f62d6e010ba005a4f4e86dbe82734a02
June <kimjune01@gmail.com>
2017-08-06 12:52:35 -0700

    refactor login

--- app/controllers/sesh_controller.rb
+++ app/controllers/sesh_controller.rb
@@ -1,34 +1,21 @@
 class SeshController < Devise::SessionsController
   protect_from_forgery except: :create
 
-
   # POST /login
   def create
-    puts "YOU MUST CONSTRUCT MORE PYLONS"
-    puts params
-    # permitted = params.require(:login).permit([:email, :password])
     permitted = params.require(:sesh).permit([:email, :password])
-    input_email = permitted[:email]
-    input_password = permitted[:password]
-    user = User.find_for_authentication(email: input_email)
+    user = User.find_for_authentication(email: permitted[:email])
+    issueBadgeIfValid(user, permitted[:password])
+  end
 
-    puts(user)
-    puts "YOU MUST CONSTRUCT MORE PYLONS"
-
-    if user.valid_password?(input_password)
-
+  def issueBadgeIfValid(user, password)
+    if user.valid_password?(password)
       user.session_token = randomToken
       user.save
-      payload = {
-          user_id: user.id,
-          token: user.session_token
-      }
-      render :json => payload, :status => 200
+      render :json => {user_id:user.id, token:user.session_token}, :status => 200
     else
       render :json => {:reason => "Invalid credentials"}, :status => 409
     end
-
-
   end
 
   def randomToken
