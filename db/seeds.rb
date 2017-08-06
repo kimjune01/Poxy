09b9bb148d15d4baa970d6a720bdd9c8524dbd84
James Leong <jyleong@sfu.ca>
2017-08-06 13:47:51 -0700

    seed with two unique users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "james.leong94@gmail.com",
+            session_token: "ABCD1234",
+            password: "mypassword",
+            password_confirmation: "mypassword")
+
+User.create(email: "alice@alice.com",
+            session_token: "12341234",
+            password: "password123",
+            password_confirmation: "password123")
