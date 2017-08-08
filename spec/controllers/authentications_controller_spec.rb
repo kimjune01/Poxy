19c428322c408aa66859a64a379df4ce60eed296
James Leong <jyleong@sfu.ca>
2017-08-08 16:38:42 -0700

    rspec uit test for find by id = 1

--- spec/controllers/authentications_controller_spec.rb
+++ spec/controllers/authentications_controller_spec.rb
@@ -8,12 +8,14 @@ RSpec.describe AuthenticationsController, type: :request do
       password: "thisisapass", 
       password_confirmation: "thisisapass")}
 
+    headers = {
+        "Accept" => "application/json",
+        "Content-Type" => "application/json"
+    }
+
     it "authenticates a valid user" do
         expect(user).not_to be_nil
-        headers = {
-            "Accept" => "application/json",
-            "Content-Type" => "application/json"
-        }
+
       post "/authenticate", params: {
           user_id: user.id, 
           session_token: "ABCDEF"
@@ -21,7 +23,45 @@ RSpec.describe AuthenticationsController, type: :request do
       expect(response.content_type).to eq("application/json")
       expect(response).to have_http_status(:ok)
     end
+
   end
+
+  describe "block for user by id" do
+    let (:user) {User.create(email: "bob@bob.com",
+                             session_token: "ABCDEF",
+                             password: "thisisapass",
+                             password_confirmation: "thisisapass")}
+    headers = {
+        "Accept" => "application/json",
+        "Content-Type" => "application/json"
+    }
+
+    it "authenticates a valid user" do
+      expect(user).not_to be_nil
+
+      post "/authenticate", params: {
+          user_id: 1,
+          session_token: "ABCDEF"
+      }.to_json, headers: headers
+      expect(response.content_type).to eq("application/json")
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "authenticates a valid user" do
+      expect(user).not_to be_nil
+
+      post "/authenticate", params: {
+          user_id: 1,
+          session_token: "WRONG TOKEN"
+      }.to_json, headers: headers
+      expect(response.content_type).to eq("application/json")
+      expect(response).to have_http_status(:bad_request)
+    end
+
+  end
+
+
+
 
 
 end
