1a0b2f4dfc1f87e07c6f439f919de924ccde799c
June <kimjune01@gmail.com>
2017-10-08 14:52:31 -0700

    static eula

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
     get '/login',  to: 'sesh#new'
   end
 
-  get "/eula" => "pages#show"
+  get "/eula" => "static#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
